d2f202d1fb6b192af055a0bd6c7b4a4db8c82f13
Lucas Davi <lucasdavisan@gmail.com>
2020-03-26 11:02:07 -0300

    3nd. example of Component and properties.

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Header(props) {
+function Header({children}) {
   return(
     <header>
-      <h1>{props.children}</h1>
+      <h1>{children}</h1>
     </header>
   );
 }
